88337d2f6c37c8fbb804423c7e0705553e6ddf47
Brandon Brockhoeft <brandon.brockhoeft@outlook.com>
2016-09-01 22:44:41 -0500

    Adding Problem1 solution and tests

---
+++ Afterman.Interview/Problem1/TicTacToeBoard.cs
@@ -0,0 +1,44 @@
+﻿namespace Afterman.Interview.Problem1
+{
+    /// <summary>
+    /// Board for modeling Tic-Tac-Toe -- not for interactive play
+    /// </summary>
+    public class TicTacToeBoard
+    {
+        public enum TicTacToePiece
+        {
+            None,
+            X,
+            O
+        }
+
+        public TicTacToePiece R0C0 { get; private set; }
+        public TicTacToePiece R0C1 { get; private set; }
+        public TicTacToePiece R0C2 { get; private set; }
+
+        public TicTacToePiece R1C0 { get; private set; }
+        public TicTacToePiece R1C1 { get; private set; }
+        public TicTacToePiece R1C2 { get; private set; }
+
+        public TicTacToePiece R2C0 { get; private set; }
+        public TicTacToePiece R2C1 { get; private set; }
+        public TicTacToePiece R2C2 { get; private set; }
+
+        public TicTacToeBoard(TicTacToePiece r0c0, TicTacToePiece r0c1, TicTacToePiece r0c2,
+            TicTacToePiece r1c0, TicTacToePiece r1c1, TicTacToePiece r1c2,
+            TicTacToePiece r2c0, TicTacToePiece r2c1, TicTacToePiece r2c2)
+        {
+            this.R0C0 = r0c0;
+            this.R0C1 = r0c1;
+            this.R0C2 = r0c2;
+
+            this.R1C0 = r1c0;
+            this.R1C1 = r1c1;
+            this.R1C2 = r1c2;
+
+            this.R2C0 = r2c0;
+            this.R2C1 = r2c1;
+            this.R2C2 = r2c2;
+        }
+    }
+}
---
+++ Afterman.Interview/Problem1/TicTacToeEvaluator.cs
@@ -0,0 +1,44 @@
+﻿namespace Afterman.Interview.Problem1
+{
+    public class TicTacToeEvaluator
+    {
+        public static TicTacToeBoard.TicTacToePiece EvaluateForWinner(TicTacToeBoard board)
+        {
+            TicTacToeBoard.TicTacToePiece winner;
+
+            if (HasWinner(board.GetRow0(), out winner))
+                return winner;
+            if (HasWinner(board.GetRow1(), out winner))
+                return winner;
+            if (HasWinner(board.GetRow2(), out winner))
+                return winner;
+
+            if (HasWinner(board.GetCol0(), out winner))
+                return winner;
+            if (HasWinner(board.GetCol1(), out winner))
+                return winner;
+            if (HasWinner(board.GetCol2(), out winner))
+                return winner;
+
+            if (HasWinner(board.GetDiagonal0(), out winner))
+                return winner;
+            if (HasWinner(board.GetDiagonal1(), out winner))
+                return winner;
+
+            return TicTacToeBoard.TicTacToePiece.None;
+        }
+
+        private static bool HasWinner(TicTacToeBoard.TicTacToePiece[] pieces, out TicTacToeBoard.TicTacToePiece winner)
+        {
+            // No array error handling or empty board check
+            if(pieces[0] == pieces[1] && pieces[1] == pieces[2])
+            {
+                winner = pieces[0];
+                return true;
+            }
+
+            winner = TicTacToeBoard.TicTacToePiece.None;
+            return false;
+        }
+    }
+}
