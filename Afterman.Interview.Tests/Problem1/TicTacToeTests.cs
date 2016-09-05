eea0ca79f3f1db1af27427afb85333968e304f15
Roderick Ford <Roderick Ford>
2016-09-05 12:22:34 -0400

    R. Ford submission

---
+++ Afterman.Interview.Tests/Problem1/TicTacToeTests.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Afterman.Interview.Problem1;
+
+namespace Afterman.Interview.Tests.Problem1
+{
+    [TestClass]
+    public class TicTacToeTests
+    {
+
+        [TestMethod]
+        public void TestColumnWins()
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                TicTacToe game = new TicTacToe();
+                for (int i = 0; i < 3; i++)
+                {
+                    Assert.IsFalse(game.WinnerExists());
+                    game.SetUserPosition(TicTacToe.PlayerNumEnum.PLAYER_ONE, i, col);
+                }
+                Assert.IsTrue(game.WinnerExists());
+                Assert.AreEqual(TicTacToe.PlayerNumEnum.PLAYER_ONE, game.GetWinner());
+            }
+        }
+
+        [TestMethod]
+        public void TestRowWins()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                TicTacToe game = new TicTacToe();
+                for (int i = 0; i < 3; i++)
+                {
+                    Assert.IsFalse(game.WinnerExists());
+                    game.SetUserPosition(TicTacToe.PlayerNumEnum.PLAYER_ONE, row, i);
+                }
+                Assert.IsTrue(game.WinnerExists());
+                Assert.AreEqual(TicTacToe.PlayerNumEnum.PLAYER_ONE, game.GetWinner());
+            }
+        }
+
+        [TestMethod]
+        public void TestDiagonalWins()
+        {
+            // check backward slash
+            {
+                TicTacToe game = new TicTacToe();
+                for (int row = 0; row < 3; row++)
+                {
+                    Assert.IsFalse(game.WinnerExists());
+                    game.SetUserPosition(TicTacToe.PlayerNumEnum.PLAYER_ONE, row, row);
+                }
+                Assert.IsTrue(game.WinnerExists());
+                Assert.AreEqual(TicTacToe.PlayerNumEnum.PLAYER_ONE, game.GetWinner());
+            }
+            // check forward slash
+            {
+                TicTacToe game = new TicTacToe();
+                for (int row = 0; row < 3; row++)
+                {
+                    Assert.IsFalse(game.WinnerExists());
+                    game.SetUserPosition(TicTacToe.PlayerNumEnum.PLAYER_ONE, row, 2 - row);
+                }
+                Assert.IsTrue(game.WinnerExists());
+                Assert.AreEqual(TicTacToe.PlayerNumEnum.PLAYER_ONE, game.GetWinner());
+            }
+        }
+
+        [TestMethod]
+        public void TestPlayerWins()
+        {
+            for (int player = 0; player < 2; player++)
+            {
+                TicTacToe.PlayerNumEnum winner = (TicTacToe.PlayerNumEnum)player;
+                TicTacToe game = new TicTacToe();
+                for (int i = 0; i < 3; i++)
+                {
+                    Assert.IsFalse(game.WinnerExists());
+                    game.SetUserPosition(winner, 0, i);
+                }
+                Assert.IsTrue(game.WinnerExists());
+                Assert.AreEqual(winner, game.GetWinner());
+            }
+        }
+
+
+        [TestMethod]
+        public void TestOutOfBounds()
+        {
+           TicTacToe.PlayerNumEnum player = TicTacToe.PlayerNumEnum.PLAYER_MAX;
+           TicTacToe game = new TicTacToe();
+           try
+           {
+               game.SetUserPosition(player, 0, 0);
+           }
+           catch (ArgumentOutOfRangeException ex)
+           {
+               StringAssert.Contains(ex.Message, TicTacToe.ArgumentOutOfRangeMessage);
+           }
+           player = TicTacToe.PlayerNumEnum.PLAYER_ONE;
+           try
+           {
+               game.SetUserPosition(player, 0, 5);
+           }
+           catch (ArgumentOutOfRangeException ex)
+           {
+               StringAssert.Contains(ex.Message, TicTacToe.ArgumentOutOfRangeMessage);
+           }
+           try
+           {
+               game.SetUserPosition(player, 5, 0);
+           }
+           catch (ArgumentOutOfRangeException ex)
+           {
+               StringAssert.Contains(ex.Message, TicTacToe.ArgumentOutOfRangeMessage);
+           }
+        }
+
+        [TestMethod]
+        public void TestAlternateSizes()
+        {
+            for (int dim = 3; dim < 10; dim++)
+            {
+                // check backward slash
+                {
+                    TicTacToe game = new TicTacToe(dim);
+                    for (int row = 0; row < dim; row++)
+                    {
+                        Assert.IsFalse(game.WinnerExists());
+                        game.SetUserPosition(TicTacToe.PlayerNumEnum.PLAYER_ONE, row, row);
+                    }
+                    Assert.IsTrue(game.WinnerExists());
+                    Assert.AreEqual(TicTacToe.PlayerNumEnum.PLAYER_ONE, game.GetWinner());
+                }
+                // check forward slash
+                {
+                    TicTacToe game = new TicTacToe(dim);
+                    for (int row = 0; row < dim; row++)
+                    {
+                        Assert.IsFalse(game.WinnerExists());
+                        game.SetUserPosition(TicTacToe.PlayerNumEnum.PLAYER_ONE, row, (dim - 1) - row);
+                    }
+                    Assert.IsTrue(game.WinnerExists());
+                    Assert.AreEqual(TicTacToe.PlayerNumEnum.PLAYER_ONE, game.GetWinner());
+                }
+            }
+        }
+
+    }
+}
---
+++ Afterman.Interview/Problem1/TicTacToe.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Afterman.Interview.Problem1
+{
+    
+    public class TicTacToe
+    {
+        // Implement an algorithm designed to determine if there is a winner in a game of Tic-Tac-Toe
+        // ==========================================================================================
+
+        public enum PlayerNumEnum
+        {
+            PLAYER_ONE,
+            PLAYER_TWO,
+            PLAYER_MAX
+        };
+
+        public const string ArgumentOutOfRangeMessage = "Method inputs are out of range";
+
+        // each player has a list of the rows in the game
+        // a row score is incremented when the user places his token in that row
+        private Dictionary<PlayerNumEnum, List<int>> RowScores = new Dictionary<PlayerNumEnum,List<int>>();
+
+        // each player has a list of the columns in the game
+        // a column score is incremented when the user places his token in that column
+        private Dictionary<PlayerNumEnum, List<int>> ColScores = new Dictionary<PlayerNumEnum, List<int>>();
+
+        // each player has a list of the two diagonals in the game
+        // a diagonal score is incremented when the user places his token in that diagonal
+        private Dictionary<PlayerNumEnum, List<int>> DiagScores = new Dictionary<PlayerNumEnum, List<int>>();
+
+        private int dimensions = 3;
+
+        // construct standard tic-tac-toe board size of 3x3
+        public TicTacToe()
+        {
+            for (int pi = 0; pi < (int)PlayerNumEnum.PLAYER_MAX; pi++)
+            {
+                PlayerNumEnum p = (PlayerNumEnum)pi;
+                this.RowScores[p] = new List<int>(new int[dimensions]);
+                this.ColScores[p] = new List<int>(new int[dimensions]);
+                this.DiagScores[p] = new List<int>(new int[2]);
+            }
+        }
+
+        // construct non-standard tic-tac-toe sizes, greater than 3x3
+        public TicTacToe(int dim)
+        {
+            this.dimensions = dim;
+            for (int pi = 0; pi < (int)PlayerNumEnum.PLAYER_MAX; pi++)
+            {
+                PlayerNumEnum p = (PlayerNumEnum)pi;
+                this.RowScores[p] = new List<int>(new int[dimensions]);
+                this.ColScores[p] = new List<int>(new int[dimensions]);
+                this.DiagScores[p] = new List<int>(new int[2]);
+            }
+        }
+
+        // Set the position of the users token, but does not check if the space was already taken
+        public void SetUserPosition(PlayerNumEnum player, int row, int col)
+        {
+            if (row < dimensions && col < dimensions && player < PlayerNumEnum.PLAYER_MAX)
+            {
+                RowScores[player][row]++;
+                ColScores[player][col]++;
+                if (row == col)
+                {
+                    DiagScores[player][0]++;
+                }
+                if (row + col == dimensions - 1)
+                {
+                    DiagScores[player][1]++;
+                }
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(ArgumentOutOfRangeMessage);
+            }
+        }
+
+        // Determines which player was the winner
+        public PlayerNumEnum GetWinner()
+        {
+            PlayerNumEnum winner = PlayerNumEnum.PLAYER_MAX;
+            for (int pi = 0; pi < (int)PlayerNumEnum.PLAYER_MAX; pi++)
+            {
+                PlayerNumEnum p = (PlayerNumEnum)pi;
+                for (int index = 0; index < dimensions; index++ )
+                {
+                    if (RowScores[p][index] == dimensions
+                        || ColScores[p][index] == dimensions)
+                    {
+                        return p;
+                    }
+                }
+                if (DiagScores[p][0] == dimensions
+                        || DiagScores[p][1] == dimensions)
+                {
+                    return p;
+                }
+            }
+
+            return winner;
+        }
+
+        // Check whether a winner currently exists
+        public bool WinnerExists()
+        {
+            return GetWinner() != PlayerNumEnum.PLAYER_MAX;
+        }
+    }
+}
